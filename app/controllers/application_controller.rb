2b9693ffea9ffb2ae518f2366da95a5f4f8a58b1
Jon Daniel <binarycleric@gmail.com>
2016-03-31 17:09:24 -0400

    Added error handling for unexpected errors
    We can wire this up to PagerDuty/Airbrake if necessary.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
 
+  rescue_from StandardError do |error|
+    Rails.logger.fatal "Something really bad happened. error: #{error.class} message: #{error.message}"
+    
+    if Rails.env.development?
+      raise error
+    else
+      render status: :not_found, json: {
+        error: {
+          message: "That wasn't supposed to happen. We'll get that fixed as soon as we can!", 
+        }
+      }
+    end
+  end 
+
   def self.handle_missing_entity
     rescue_from ActiveRecord::RecordNotFound do
       render status: :not_found, json: {
